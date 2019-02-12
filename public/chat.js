9654e2f259ec92b3411a4981d89b8a129ecf7680
Saud Tauqeer <saudtauqeer381@gmail.com>
2019-02-12 21:41:33 +0500

    Message is rendering.

--- public/chat.js
+++ public/chat.js
@@ -7,9 +7,18 @@ const btn = document.getElementById("send");
 const output = document.getElementById("output");
 const feedback = document.getElementById("feedback");
 
-btn.onclick(() => {
+btn.addEventListener("click", () => {
   socket.emit("chat", {
     message: message.value,
     name: handle.value
   });
+  message.value = "";
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("typing", handle.value);
+});
+
+socket.on("chat", data => {
+  output.innerHTML += `<p><strong>${data.name}</strong>: ${data.message} </p>`;
 });
